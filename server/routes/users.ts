0816129a4057201071a289fb6e8e7e4ffc88c64b
ravidassnz <ravidassnz@gmail.com>
2025-11-07 14:30:58 +1300

    accidently made changes to users.ts instead of posts.ts. have moved codes to proper files

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -2,50 +2,39 @@ import { Router } from 'express'
 import checkJwt, { JwtRequest } from '../auth0.ts'
 import { StatusCodes } from 'http-status-codes'
 
-import * as db from '../db/posts.ts'
+import * as db from '../db/fruits.ts'
 
 const router = Router()
 
 router.get('/', async (req, res) => {
   try {
-    const posts = await db.getAllPosts()
+    const fruits = await db.getAllFruits()
 
- res.json({ posts })
+    res.json({ fruits: fruits.map((fruit) => fruit.name) })
   } catch (error) {
     console.log(error)
-    res.status(500).json({ message: 'Something went wrong with getting all posts' })
+    res.status(500).json({ message: 'Something went wrong' })
   }
 })
 
 router.get('/:id', async (req, res, next) => {
   try {
-    const post = await db.getPostById(req.params.id)
-    res.json(post)
+    const fruit = await db.getFruitById(req.params.id)
+    res.json(fruit)
   } catch (err) {
     next(err)
   }
 })
 
 router.post('/', checkJwt, async (req: JwtRequest, res, next) => {
-  
-// const userId = req.auth?.sub  - to add this once we have users and confirm the name of column
-  if (!req.auth?.sub ) {   // swap with if (!userId)
+  if (!req.auth?.sub) {
     res.sendStatus(StatusCodes.UNAUTHORIZED)
     return
   }
 
   try {
-    const { title, content, mediaType, mediaURL } = req.body  // make sure this matches
-    
-    const newPost = {
-      userId: userId, // CRITICAL: Absolutely need this!!!
-      title,
-      content,
-      mediaType,
-      mediaURL,
-      // i don't think we need to have timestamps here, but to test. added as CRITICAL just in caseS
-    }    
-    const id = await db.addPost(newPost)
+    const { owner, name } = req.body
+    const id = await db.addFruit({ owner, name })
     res
       .setHeader('Location', `${req.baseUrl}/${id}`)
       .sendStatus(StatusCodes.CREATED)
