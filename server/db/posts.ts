89774f80921b0d012824ade3667151e7bdc68a70
ravidassnz <ravidassnz@gmail.com>
2025-11-07 14:39:47 +1300

    change posts.ts in db to be plural 'posts', was 'post'

--- server/db/posts.ts
+++ server/db/posts.ts
@@ -2,16 +2,16 @@ import db from './connection.ts'
 import { Post } from '../../models/posts'
 
 export async function getAllPosts() {
-  const post = await db('post').select()
+  const post = await db('posts').select()
   return post as Post[]
 }
 
 export async function getPostById(id: number | string) {
-  const post = await db('post').select().first().where({ id })
+  const post = await db('posts').select().first().where({ id })
   return post as Post
 }
 
 export async function addPost(data: postData) {
-  const [id] = await db('post').insert(data)
+  const [id] = await db('posts').insert(data)
   return id
 }
