8338930f816f46a8a15b02ce59d79b034443b11a
Aimee Eastwood <aimeejeastwood@gmail.com>
2025-11-08 02:59:15 +1300

    nother little fix

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -3,20 +3,23 @@ import NavBar from './NavBar'
 
 export default function Layout() {
   return (
-    <div className="flex min-h-screen flex-col">
-      <NavBar />
-
+    <>
       <header className="p-4 text-center text-xl font-bold">
         <h1>DevConnect</h1>
+        {/* (optional header content here) */}
       </header>
 
-      <main className="flex-1 p-4">
+      <nav>
+        <NavBar />
+      </nav>
+
+      <main className="p-4">
         <Outlet />
       </main>
 
       <footer className="p-4 text-center text-xs opacity-60">
-        © DevConnect Team
+        {/* Footer content here later */}
       </footer>
-    </div>
+    </>
   )
 }
