d6e95e4c89aa6b39d5a022553d56cfc2a81da0b0
Sasha Chambers <sashachambers10.12@gmail.com>
2025-11-09 11:28:04 +1300

    Linked to User routes

--- server/server.ts
+++ server/server.ts
@@ -2,12 +2,14 @@ import express from 'express'
 import * as Path from 'node:path'
 
 import postRoutes from './routes/posts.ts'
+import userRoutes from './routes/users.ts'
 
 const server = express()
 
 server.use(express.json())
 
 server.use('/api/v1/posts', postRoutes)
+server.use('/api/v1/users', userRoutes)
 
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
