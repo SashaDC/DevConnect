b06465d27cc0b354240317de19d3606aa60fa3a5
Lucas Spilhaus <lucasspilhausnz@gmail.com>
2025-11-09 06:45:29 +1300

    Set up basic routing with HomeFeed as a placeholder

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,3 +1,9 @@
 import { createRoutesFromElements, Route } from 'react-router'
 import App from './components/App.tsx'
-export default createRoutesFromElements(<Route index element={<App />} />)
+import HomeFeed from './components/HomeFeed.tsx'
+
+export default createRoutesFromElements(
+    <Route path="/" element={<App />}>
+        <Route index element={<HomeFeed />} />
+    </Route>
+)
