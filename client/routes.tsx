bbd9c7c9e203d01fbe0a9abafdd5a49cf3e7590e
Lucas Spilhaus <lucasspilhausnz@gmail.com>
2025-11-09 13:05:58 +1300

    cleaned up update user info work

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,11 +1,9 @@
 import { createRoutesFromElements, Route } from 'react-router'
 import App from './components/App.tsx'
 import HomeFeed from './components/HomeFeed.tsx'
-import Profile from './components/Profile.tsx'
 
 export default createRoutesFromElements(
     <Route path="/" element={<App />}>
         <Route index element={<HomeFeed />} />
-        <Route path="/temp" element={<Profile />} />
     </Route>
 )
